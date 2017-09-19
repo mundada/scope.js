3afb4318a57a12162367c232cbb115e92ab2c805
ashish mundada <amundada04@gmail.com>
2017-09-19 18:05:08 -0500

    new file test

---
+++ this.js
@@ -0,0 +1,21 @@
+funtion grade()
+{
+    var a = 90;
+    funtion grade2()
+    {
+    var b =80;
+    console.log(a);
+    console.log(b);
+    }
+    grade();
+    
+    funtion grade3()
+    {
+     var c =70;
+   console.log(a);
+   console.log(b);
+   console.log(c);
+   }
+grade3();
+
+}
